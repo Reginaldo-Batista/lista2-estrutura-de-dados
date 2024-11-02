596ab5bcfaf65d7ff68a46f70bd945438b4e2530
Reginaldo Batista <reginaldovlog@gmail.com>
2024-11-02 18:03:25 -0300

    Adicionado: Tree.localizarNoIncompleto(Boolean, int, Node)

--- classes/Tree.java
+++ classes/Tree.java
@@ -1,5 +1,7 @@
 package classes;
 
+import java.util.Random;
+
 class Node {
 
     int valor;
@@ -27,7 +29,26 @@ public class Tree {
         } else {
             refNo.esquerda = novoNode;
         }
-        
+
+    }
+
+    public Node localizarNoIncompleto() {
+
+        Random random = new Random();
+        Node nodeAlvo = this.raiz;
+        Boolean existeEsquerda = nodeAlvo.direita != null;
+        Boolean existeDireita = nodeAlvo.esquerda != null;
+
+        while (existeEsquerda && existeDireita) {
+            if (random.nextBoolean()) {
+                nodeAlvo = nodeAlvo.direita;
+            } else {
+                nodeAlvo = nodeAlvo.esquerda;
+            }
+        }
+
+        return nodeAlvo;
+
     }
     
 }
