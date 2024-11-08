9c5aef83c4c7b2f0742ac650634e6c330048daac
Reginaldo Batista <reginaldovlog@gmail.com>
2024-11-08 13:14:10 -0300

    classe Node agora está num arquivo próprio

---
+++ classes/Node.java
@@ -0,0 +1,56 @@
+package classes;
+
+public class Node {
+
+    private int valor;
+    private Node esquerda;
+    private Node direita;
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public Node getEsquerda() {
+        return esquerda;
+    }
+
+    public void setEsquerda(Node esquerda) {
+        this.esquerda = esquerda;
+    }
+
+    public Node getDireita() {
+        return direita;
+    }
+
+    public void setDireita(Node direita) {
+        this.direita = direita;
+    }
+
+    public Node(int valor) {
+        this.valor = valor;
+        this.esquerda = null;
+        this.direita = null;
+    }
+
+    public boolean hasLeft() {
+        return (this.esquerda != null);
+    }
+
+    public boolean hasRight() {
+        return (this.direita != null);
+    }
+
+    public boolean isLeaf() {
+        return (this.esquerda == null && this.direita == null);
+    }
+
+    @Override
+    public String toString() {
+        return "" + this.valor;
+    }
+
+}
