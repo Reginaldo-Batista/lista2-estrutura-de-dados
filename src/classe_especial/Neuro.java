2e4d12c0129ac269b4bf1650da459bed9c99944c
Reginaldo Batista <reginaldovlog@gmail.com>
2024-11-14 21:28:02 -0300

    criada a classe Neuro

---
+++ src/classe_especial/Neuro.java
@@ -0,0 +1,12 @@
+package src.classe_especial;
+
+import src.classes.Node;
+
+public class Neuro extends Criteria {
+
+    @Override
+    public Node apply(Node refNo, int valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+}
