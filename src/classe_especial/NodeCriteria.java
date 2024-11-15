438db5523de9a7fae75447a4a09754a98f189cfe
Reginaldo Batista <reginaldovlog@gmail.com>
2024-11-14 21:29:00 -0300

    criando NodeCriteria que extende Node

---
+++ src/classe_especial/NodeCriteria.java
@@ -0,0 +1,13 @@
+package src.classe_especial;
+
+import src.classes.Node;
+
+public class NodeCriteria extends Node {
+
+    Peso peso;
+
+	public NodeCriteria(int valor) {
+		super(valor);
+	}
+	
+}
